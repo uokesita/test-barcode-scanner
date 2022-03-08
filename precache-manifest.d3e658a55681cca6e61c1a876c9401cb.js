84e08f45a92d6c453ac8ae235ec35331f86dcb78
Osledy Bazó <hola@alamedadev.com>
2022-03-08 11:45:48 -0500

    Updates

---
+++ precache-manifest.d3e658a55681cca6e61c1a876c9401cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f9f9ef977438e469b06741618b7b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b1c1f12e554c7d9805b",
+    "url": "/static/css/main.e3155fda.chunk.css"
+  },
+  {
+    "revision": "4bfa208e1071d60273d3",
+    "url": "/static/js/2.7f4b5dc8.chunk.js"
+  },
+  {
+    "revision": "7e807c4d0a63561b711f4cda4dbd88c7",
+    "url": "/static/js/2.7f4b5dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1c1f12e554c7d9805b",
+    "url": "/static/js/main.e27b0114.chunk.js"
+  },
+  {
+    "revision": "7d3c3ab3fc37d6173ff1",
+    "url": "/static/js/runtime-main.438aecb5.js"
+  },
+  {
+    "revision": "6bb58878bf0578b2d5d22e4ba83ff49c",
+    "url": "/static/media/beep-07.6bb58878.wav"
+  }
+]);
